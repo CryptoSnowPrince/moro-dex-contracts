83d30c552aab295af230b2f1b6387eb61173597a
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-24 04:45:23 +0200

    Add autoswapper

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,34 +26,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.6.12",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000,
-          },
-        },
-      },
-      {
-        version: "0.8.0",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000,
-          },
-        },
-      },
-      {
-        version: "0.8.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 2000,
-          },
-        },
-      },
-      {
-        version: "0.8.7",
+        version: "0.8.17",
         settings: {
           optimizer: {
             enabled: true,
