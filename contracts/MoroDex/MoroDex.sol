f41680a4bcc32f478a85fa6f33c6b9cbca11e295
CryptoSnowPrince <topdirector2017@gmail.com>
2023-04-24 12:43:09 +0200

    update token info

--- contracts/MoroDex/MoroDex.sol
+++ contracts/MoroDex/MoroDex.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 /**
- * @title MorodexToken (MDEX), ERC-20 token
+ * @title MORODEX (MDEX), ERC-20 token, 18, 1,000,000,000
  * @notice Inherit from the ERC20Permit, allowing to sign approve off chain
  */
-contract MorodexToken is ERC20Permit {
+contract MoroDex is ERC20Permit {
     constructor(string memory _name, string memory _symbol, uint256 _supply) ERC20(_name, _symbol) ERC20Permit(_name) {
         _mint(msg.sender, _supply);
     }
